773d6e7ddfa9659e0358650027441af43ccb5045
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-13 14:46:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,10 @@ def batch_badge_creator(names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "L
    names << "Hello, my name is #{name}."
   end 
 end
+
+def assign_rooms 
+  rooms [1,2,3,4,5,6,7]
+  rooms.each do |room|
+    puts "Hello, #{name}! You'll be assiggned to room #{room}!"
+  end
+end
